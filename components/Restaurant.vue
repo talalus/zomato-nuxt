6b3256d0a9efa0f85785948c1f228ca695dca176
talalus <talal.suhail@snow.dog>
2019-11-23 18:07:27 +0200

    add basic style and style restaurant component

--- components/Restaurant.vue
+++ components/Restaurant.vue
@@ -1,18 +1,76 @@
 <template>
   <div>
-    <pre> {{ restaurant }}</pre>
+    <div class="restaurant">
+      <div class="restaurant__image">
+        <img :src="restaurant.featured_image" :alt="restaurant.name">
+      </div>
+      <div class="restaurant__details">
+        <h1 class="restaurant__name">
+          {{ restaurant.name }}
+        </h1>
+        <div class="restaurant__address">
+          {{ restaurant.location.address }}
+        </div>
+        <div class="restaurant__options">
+          <div class="restaurant__option">
+            <span
+              :class="[
+              'restaurant__option-status',
+              restaurant.has_table_booking ? 'restaurant__option-status--green': 'restaurant__option-status--red'
+              ]"
+            >
+            </span>
+            <span class="restaurant__option-detail" v-if="restaurant.has_table_booking">Bookings available</span>
+            <span class="restaurant__option-detail" v-else>No bookings</span>
+          </div>
+          <div class="restaurant__option">
+            <span
+              :class="[
+              'restaurant__option-status',
+              restaurant.has_online_delivery ? 'restaurant__option-status--green': 'restaurant__option-status--red'
+              ]"
+            >
+            </span>
+            <span class="restaurant__option-detail" v-if="restaurant.has_online_delivery">Delivery available</span>
+            <span class="restaurant__option-detail" v-else>No delivery</span>
+          </div>
+        </div>
+        <div class="restaurant__attribute">
+          <div class="restaurant__attribute-title">Cuisines</div>
+          <div class="restaurant__attribute-value">
+            {{ restaurant.cuisines }}
+          </div>
+        </div>
+        <div class="restaurant__attribute">
+          <div class="restaurant__attribute-title">
+            Phone numbers
+          </div>
+          <div class="restaurant__attribute-value">
+            {{ restaurant.phone_numbers}}
+          </div>
+        </div>
+        <div class="restaurant__attribute">
+          <div class="restaurant__attribute-title">
+            Opening hours
+          </div>
+          <div class="restaurant__attribute-value">
+            <span>{{ restaurant.timings }}</span>
+            <span class="restaurant__attribute-badge">Open Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    restaurant: {
-      type: Object,
-      required: true
+  export default {
+    props: {
+      restaurant: {
+        type: Object,
+        required: true
+      }
     }
   }
-}
 </script>
 
-<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,57 @@
 <template>
-  <div class="container">
-    <categories :categories="categories" />
-    <categories :categories="cuisines" />
-    <restaurants :restaurants="restaurants" />
-    <nuxt-child />
+  <div class="grid">
+    <div class="grid__row">
+      <!--<categories :categories="categories"/>-->
+      <!--<categories :categories="cuisines"/>-->
+    </div>
+    <div class="grid__row grid__row--dark">
+      <restaurants class="grid__col grid__col--4" :restaurants="restaurants"/>
+      <div class="grid__col grid__col--8">
+        <nuxt-child/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Categories from '~/components/Categories'
-import Restaurants from '~/components/Restaurants'
+  import Categories from '~/components/Categories'
+  import Restaurants from '~/components/Restaurants'
 
-export default {
-  components: {
-    Categories,
-    Restaurants
-  },
-  computed: {
-    categories() {
-      return this.$store.state.categories
+  export default {
+    components: {
+      Categories,
+      Restaurants
     },
-    cuisines() {
-      return this.$store.state.cuisines
+    computed: {
+      categories() {
+        return this.$store.state.categories
+      },
+      cuisines() {
+        return this.$store.state.cuisines
+      },
+      restaurants() {
+        return this.$store.state.restaurants
+      }
     },
-    restaurants() {
-      return this.$store.state.restaurants
-    }
-  },
-  async fetch({ store, error }) {
-    try {
-      await Promise.all([
-        store.dispatch('getCategories'),
-        store.dispatch('getCuisines'),
-        store.dispatch('search')
-      ])
-    } catch (e) {
-      error({ statusCode: 404, message: 'Page not found' })
-    }
-  },
-  methods: {
-    async search() {
-      await this.$store.dispatch('search')
+    async fetch({ store, error }) {
+      try {
+        await Promise.all([
+          store.dispatch('getCategories'),
+          store.dispatch('getCuisines'),
+          store.dispatch('search')
+        ])
+      } catch (e) {
+        error({ statusCode: 404, message: 'Page not found' })
+      }
+    },
+    methods: {
+      async search() {
+        await this.$store.dispatch('search')
+      }
     }
   }
-}
 </script>
+
+<style lang="scss">
+
+</style>
